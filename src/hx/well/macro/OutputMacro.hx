3d276630a9434af9dc4f64e303ab756357b74cb6
Barış Yıldırım <fb_bcy@hotmail.com>
2025-02-27 14:54:35 +0300

    fix jvm writeBytes performance https://github.com/HaxeFoundation/haxe/pull/11944

--- src/hx/well/macro/OutputMacro.hx
+++ src/hx/well/macro/OutputMacro.hx
@@ -2,6 +2,7 @@ package hx.well.macro;
 import haxe.macro.Expr.Field;
 import haxe.macro.Expr;
 import haxe.macro.Context;
+import haxe.io.Bytes;
 class OutputMacro {
     public static function build():Array<Field> {
         var localClass = Context.getLocalClass();
@@ -22,12 +23,52 @@ class OutputMacro {
             patchFunction(fields, "writeByte");
         }else if(localClassName == "NativeOutput")
         {
+            #if jvm
+            createWriteBytes(fields);
+            #end
             patchFunction(fields, "writeByte");
             patchFunction(fields, "writeBytes");
         }
 
         return fields;
     }
+
+    #if jvm
+    private static function createWriteBytes(fields:Array<Field>):Void {
+        var writeByteField = fields.filter(field -> field.name == "writeBytes")[0];
+        if(writeByteField != null)
+            return;
+
+        var func:Function = {
+            args: [
+                { name: "s", type: macro : Bytes },
+                { name: "pos", type: macro : Int },
+                { name: "len", type: macro : Int }
+            ],
+            ret: macro : Int,
+            expr: macro {
+                if (pos < 0 || len < 0 || pos + len > s.length)
+                    throw haxe.io.Error.OutsideBounds;
+                try {
+                    stream.write(s.getData(), pos, len);
+                } catch (e:EOFException) {
+
+                    throw new Eof();
+                } catch (e:IOException) {
+                    throw haxe.io.Error.Custom(e);
+                }
+                return len;
+            }
+        };
+
+        fields.push({
+            name: "writeBytes",
+            access: [Access.APublic, Access.AOverride],
+            kind: FieldType.FFun(func),
+            pos: Context.currentPos()
+        });
+    }
+    #end
 
     private static function patchFunction(fields:Array<Field>, name:String):Void
     {
