d595c3323efbd9ae08d5aa19b4396d792d898004
Barış Yıldırım <fb_bcy@hotmail.com>
2025-03-06 00:17:23 +0300

    ThreadLocal fix for targets that do not support Thread

--- src/haxe/ThreadLocal.hx
+++ src/haxe/ThreadLocal.hx
@@ -1,4 +1,6 @@
 package haxe;
+
+#if (target.threaded)
 import haxe.ds.WeakMap;
 import sys.thread.Thread;
 import hx.concurrent.collection.SynchronizedMap;
@@ -37,3 +39,36 @@ class ThreadLocal<T> {
         return elements.get(currentThread);
     }
 }
+#else
+class ThreadLocal<T> {
+    public var element:T = null;
+    private var initialValue:Void->T;
+    private var removeValue:T->Void;
+
+    public function new(?initialValue:Void->T, ?removeValue:T->Void) {
+        this.initialValue = initialValue;
+        this.removeValue = removeValue;
+    }
+
+    public function remove():Void {
+        if(element != null) {
+
+            if(removeValue != null)
+                removeValue(element);
+
+            element = null;
+        }
+    }
+
+    public function set(value:T) {
+        element = value;
+    }
+
+    public function get():T {
+        if(element == null && initialValue != null)
+            element = initialValue();
+
+        return element;
+    }
+}
+#end
