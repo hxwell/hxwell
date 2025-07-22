c918947f4fb27d2f4dd60be939d081bd22b3b7c9
Barış Yıldırım <fb_bcy@hotmail.com>
2025-07-22 20:12:08 +0300

    add pool size

--- tools/src/hx/well/console/HostProjectCommand.hx
+++ tools/src/hx/well/console/HostProjectCommand.hx
@@ -47,7 +47,7 @@ class HostProjectCommand extends AbstractCommand<Bool> {
         SocketInstance.builder()
             .setHost(host)
             .setPort(availablePort)
-            .setPoolSize(Std.parseInt(getOption(("poolSize"))))
+            .setPoolSize(Std.parseInt(getOption("poolSize", "2")))
             .setOnStart(() -> System.openURL('http://${displayIp}:${availablePort}'))
             .build()
             .driver()
