74b4aaf68884c4bafded94c974bd2d02e227eabf
Barış Yıldırım <fb_bcy@hotmail.com>
2025-07-25 23:22:45 +0300

    remove unused import

--- doc/src/hx/well/config/ProviderConfig.hx
+++ doc/src/hx/well/config/ProviderConfig.hx
@@ -1,6 +1,5 @@
 package hx.well.config;
 import hx.well.provider.AbstractProvider;
-import hx.well.provider.CLIProvider;
 class ProviderConfig {
     public static function get():Array<Class<AbstractProvider>> {
         return [];
