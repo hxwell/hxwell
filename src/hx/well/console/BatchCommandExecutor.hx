f8e2108a3ad6ed7ce80ab3a5ceba56b440708952
barisyild <fb_bcy@hotmail.com>
2025-07-29 17:23:16 +0300

    fix

--- src/hx/well/console/BatchCommandExecutor.hx
+++ src/hx/well/console/BatchCommandExecutor.hx
@@ -1,18 +1,18 @@
 package hx.well.console;
 class BatchCommandExecutor {
-    private var commands:Array<Class<AbstractCommand>>  = [];
+    private var commands:Array<Class<AbstractCommand<Any>>>  = [];
 
     public function new() {
 
     }
 
-    public function addCommand(commandClass:Class<AbstractCommand>):Void {
+    public function addCommand(commandClass:Class<AbstractCommand<Any>>):Void {
         commands.push(commandClass);
     }
 
     public function execute():Void {
         while (commands.length > 0) {
-            var command:Class<AbstractCommand> = commands.shift();
+            var command:Class<AbstractCommand<Any>> = commands.shift();
             try {
                 Type.createInstance(command, []).handle();
             } catch (e:Dynamic) {
