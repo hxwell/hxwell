784b814d3ab6cdc4681420de1a1248140a2785a8
barisyild <fb_bcy@hotmail.com>
2025-08-27 17:06:16 +0300

    Fix insert query builder

--- src/hx/well/database/query/InsertQueryBuilder.hx
+++ src/hx/well/database/query/InsertQueryBuilder.hx
@@ -8,15 +8,16 @@ class InsertQueryBuilder {
      * @param keys An iterator for the column names to insert data into.
      * @return A formatted SQL INSERT string.
      */
-    public static function toString<T>(query:QueryBuilder<T>, keys:Iterator<String>):String {
+    public static function toString<T>(query:QueryBuilder<T>, iterKeys:Iterator<String>):String {
         if (query.conditions.length > 0) throw new Exception("INSERT statements do not support WHERE clauses.");
         if (query.joins.length > 0) throw new Exception("INSERT statements do not support JOINs.");
         if (query.orderByClause != "") throw new Exception("INSERT statements do not support ORDER BY.");
         if (query.limitValue != -1) throw new Exception("INSERT statements do not support LIMIT.");
-        if (!keys.hasNext()) throw new Exception("Cannot perform an INSERT with no columns specified.");
+        if (!iterKeys.hasNext()) throw new Exception("Cannot perform an INSERT with no columns specified.");
 
-        var columns = [for (key in keys) key].join(", ");
-        var placeholders = [for (_ in 0...columns.length) "?"].join(", ");
+        var keys:Array<String> = [for (key in iterKeys) key];
+        var columns = keys.join(", ");
+        var placeholders = [for (_ in 0...keys.length) "?"].join(", ");
 
         var stringBuf = new StringBuf();
         stringBuf.add("INSERT INTO ");
