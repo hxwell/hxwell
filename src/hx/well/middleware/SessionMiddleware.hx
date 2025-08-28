2f7a7e5f7290badab3dde2508bf7eca861a46873
barisyild <fb_bcy@hotmail.com>
2025-08-29 02:47:31 +0300

    session encryption and lifetime implemented

--- src/hx/well/middleware/SessionMiddleware.hx
+++ src/hx/well/middleware/SessionMiddleware.hx
@@ -11,11 +11,29 @@ import hx.well.http.ResponseStatic;
 import hx.well.http.RequestStatic.request;
 import hx.well.http.Response;
 import hx.well.http.Response;
+import hx.well.facades.Crypt;
+import hx.well.facades.Environment;
+import hx.well.facades.Config;
+import haxe.CallStack;
 
 class SessionMiddleware extends AbstractMiddleware {
     public function handle(request:Request, next:Request->Null<Response>):Null<Response> {
 
-        var sessionKey:Null<String> = request.cookie("sessionKey");
+        var sessionCookieKey:String = '${Environment.get("APP_NAME")}_session';
+        var encryptedSessionData:Null<String> = request.cookie(sessionCookieKey);
+        var sessionKey:String = null;
+        try {
+            if(encryptedSessionData != null) {
+                var sessionData:{key:String, createdAt:Float, type:String} = Crypt.decrypt(encryptedSessionData);
+                var sessionLifeTimeSeconds:Int = Config.get("session.lifetime") * 60;
+                if(sessionData.type == "cookie" && sessionData.createdAt + sessionLifeTimeSeconds > Math.floor(Date.now().getTime() / 1000))
+                {
+                    sessionKey = sessionData.key;
+                }
+            }
+        } catch (e) {
+            sessionKey = null;
+        }
 
         var currentSession:ISession = new Session();
         currentSession.data = Cache.store(FileSystemSessionCacheStore).get('session.${sessionKey}', new Map());
@@ -23,13 +41,12 @@ class SessionMiddleware extends AbstractMiddleware {
         if(sessionKey == null)
         {
             currentSession.sessionKey = generateSessionKey();
-            ResponseStatic.cookie("sessionKey", currentSession.sessionKey);
         }else{
             currentSession.sessionKey = sessionKey;
         }
 
+        ResponseStatic.cookie(sessionCookieKey, Crypt.encrypt({key: currentSession.sessionKey, createdAt: Math.floor(Date.now().getTime() / 1000), type: "cookie"}, true));
         request.session = currentSession;
-
         return next(request);
     }
 
