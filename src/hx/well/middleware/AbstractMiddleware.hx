d82d56fb71a4ebc020bda503fdedd799fa982a10
barisyild <fb_bcy@hotmail.com>
2025-08-30 02:37:45 +0300

    remove unused imports in AbstractMiddleware

--- src/hx/well/middleware/AbstractMiddleware.hx
+++ src/hx/well/middleware/AbstractMiddleware.hx
@@ -1,9 +1,5 @@
 package hx.well.middleware;
 import hx.well.http.Request;
-import hx.well.exception.AbortException;
-import hx.well.http.RequestStatic;
-import hx.well.model.User;
-import sys.net.Socket;
 import hx.well.http.Response;
 import hx.well.http.ResponseStatic;
 abstract class AbstractMiddleware {
