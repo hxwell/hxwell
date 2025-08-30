4f7d8e64863d9af1e95d1701933240f99d17a840
barisyild <fb_bcy@hotmail.com>
2025-08-30 05:32:15 +0300

    remove bcrypt support from Java implementation: delete `BCrypt` extern class, remove platform-specific bcrypt logic from `Hash` facade, and delete related Java library files

--- src/hx/well/facades/Hash.hx
+++ src/hx/well/facades/Hash.hx
@@ -5,8 +5,6 @@ class Hash {
     public static #if php inline #end function make(password:String, rounds:Int = 12):String {
         #if php
         return untyped php.Syntax.code("password_hash({0}, PASSWORD_BCRYPT, ['cost' => {1}])", password, rounds);
-        #elseif java
-        return Bytes.ofData(cast at.favre.lib.crypto.bcrypt.BCrypt.withDefaults().hash(rounds, cast Bytes.ofString(password).getData())).toString();
         #else
         var salt = BCrypt.generateSalt(rounds, BCrypt.Revision2B);
         return BCrypt.encode(password, salt);
@@ -17,8 +15,6 @@ class Hash {
     public static inline function check(plaintext:String, hash:String):Bool {
         #if php
         return untyped php.Syntax.code("password_verify({0}, {1})", plaintext, hash);
-        #elseif java
-        return at.favre.lib.crypto.bcrypt.BCrypt.verifyer().verify(cast Bytes.ofString(plaintext).getData(), cast Bytes.ofString(hash).getData()).verified;
         #else
         return BCrypt.verify(plaintext, hash);
         #end
